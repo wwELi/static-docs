a13070cbf3e15fa73390e80ad8ab6d8870c5142e
wei.wang <wei.wang@thoughtworks.com>
2023-10-25 17:53:30 +0800

    :art: fix file not find

--- app/index.js
+++ app/index.js
@@ -55,3 +55,37 @@ fs.writeFileSync(getOutputPathByFilename("styles.css"), result.css, "utf-8");
 
 
 
+/**
+ * copy assets
+ */
+
+
+const fspromises = require('fs').promises;
+async function copyDirectory(sourceDir, targetDir) {
+  try {
+    // 获取源目录中的所有文件和子目录
+    const files = await fspromises.readdir(sourceDir);
+
+    // 逐个处理文件和子目录
+    for (const file of files) {
+      const sourcePath = path.join(sourceDir, file);
+      const targetPath = path.join(targetDir, file);
+
+      const stat = await fspromises.stat(sourcePath);
+
+      if (stat.isFile()) {
+        // 如果是文件，则复制文件
+        await fspromises.copyFile(sourcePath, targetPath);
+        console.log(`Copied file: ${sourcePath} to ${targetPath}`);
+      } else if (stat.isDirectory()) {
+        // 如果是目录，则递归调用复制目录函数
+        await fspromises.mkdir(targetPath, { recursive: true });
+        await copyDirectory(sourcePath, targetPath);
+      }
+    }
+  } catch (error) {
+    console.error(`Error copying directory: ${error}`);
+  }
+}
+
+copyDirectory(path.join(__dirname, "../html/assets"), output_path)
